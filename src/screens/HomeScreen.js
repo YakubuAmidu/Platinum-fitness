ec02133fac922849bb4047dc3c1e79206176f054
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-27 23:34:27 -0700

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, ImageBackground, Text, StyleSheet } from "react-native";
+import ImageOne from "../assets/ImageOne.png";
 
 class HomeScreen extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>HomeScreen()</Text>
+        <ImageBackground source={ImageOne} style={styles.image}>
+          <Text>Hello</Text>
+        </ImageBackground>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+});
 
 export default HomeScreen;

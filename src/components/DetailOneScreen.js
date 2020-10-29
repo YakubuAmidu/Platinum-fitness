8e5af5bcebe2452d8dde116c4ce6fad6ebd520f7
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-28 22:02:10 -0700

    Commiting changes

--- src/components/DetailOneScreen.js
+++ src/components/DetailOneScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { EvilIcons } from "@expo/vector-icons";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import ImageFour from "../assets/ImageFour.png";
 import ImageFive from "../assets/ImageFive.png";
@@ -11,14 +12,40 @@ class DetailOneScreen extends Component {
 
         <View style={styles.viewStyleOne}>
           <ImageBackground source={ImageFour} style={styles.backgroundStyleOne}>
-            <Text style={styles.textStyleTwo}> Hi</Text>
+            <View style={styles.viewStyleTwo}>
+              <Text style={styles.textStyleTwo}> Time 45:23</Text>
+              <Text style={styles.textStyleTwo}> Distance 12,31 km</Text>
+              <Text style={styles.textStyleTwo}> Calories 133,2 kcal</Text>
+            </View>
           </ImageBackground>
           <ImageBackground source={ImageFive} style={styles.backgroundStyleTwo}>
-            <Text style={styles.textStyleThree}>Hello</Text>
+            <View style={styles.viewStyleTwo}>
+              <Text style={styles.textStyleTwo}>Time 45:23</Text>
+              <Text style={styles.textStyleTwo}>Distance 12,31 km</Text>
+              <Text style={styles.textStyleTwo}>Calories 133,2 kcal</Text>
+            </View>
           </ImageBackground>
         </View>
 
-        <Text>Hello</Text>
+        <View style={styles.viewStyleThree}>
+          <Text style={styles.textSThree}>UPGRADE TO PRO</Text>
+          <Text style={styles.textStyleFour}>
+            Enter and record all your activities
+          </Text>
+          <Text style={styles.textStyleFive}>
+            From strength and endurance to mobility and yoga--featuring our
+            trainers
+          </Text>
+        </View>
+
+        <View style={styles.viewStyleFour}>
+          <EvilIcons name="star" size={24} color="black" />
+          <View style={styles.viewStyleFive}>
+            <Text style={styles.viewStyleSix}>Pro Account</Text>
+            <Text style={styles.viewStyleSeven}>Monthly payment</Text>
+          </View>
+          <Text style={styles.viewStyleEight}>$12</Text>
+        </View>
       </View>
     );
   }
@@ -27,6 +54,8 @@ class DetailOneScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingRight: 20,
+    paddingLeft: 20,
     flexDirection: "column",
   },
   textStyleOne: {
@@ -40,29 +69,30 @@ const styles = StyleSheet.create({
   },
   viewStyleOne: {
     alignItems: "center",
-    paddingRight: 20,
-    paddingLeft: 20,
   },
   backgroundStyleOne: {
     alignSelf: "flex-end",
+    paddingLeft: 20,
+    paddingTop: 90,
     width: 300,
     height: 150,
     marginBottom: 25,
   },
+  viewStyleTwo: {
+    width: 110,
+    flexDirection: "row",
+  },
   textStyleTwo: {
-    color: "#fff",
+    color: "red",
     fontSize: 20,
     fontWeight: "bold",
   },
   backgroundStyleTwo: {
     alignSelf: "flex-start",
+    paddingRight: 20,
+    paddingTop: 90,
     width: 300,
     height: 150,
-  },
-  textStyleThree: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold",
   },
 });
 

e2f54ff2a82f27357b62ab644276d43f1f04dd5c
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-29 19:19:27 -0700

    Commiting changes

--- src/components/DetailTwoScreen.js
+++ src/components/DetailTwoScreen.js
@@ -4,11 +4,43 @@ import { View, Text, StyleSheet } from "react-native";
 class DetailTwoScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>DetailTwoScreen</Text>
+      <View style={styles.viewStyleOne}>
+        <Text style={styles.textStyleOne}>THURSDAY, 12 APRIL</Text>
+        <View style={styles.viewStyleTwo}>
+          <Text style={styles.textStyleTwo}>My Day</Text>
+          <Text style={styles.textStyleThree}>+</Text>
+        </View>
+
+        <View style={styles.viewStyleThree}>
+          <Text style={styles.textStyleFour}>
+            7 activities this week. Good job!
+          </Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  viewStyleOne: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 20,
+  },
+  textStyleOne: {
+    fontSize: 15,
+    color: "gray",
+    paddingBottom: 10,
+  },
+  viewStyleTwo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 10,
+  },
+  textStyleTwo: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+});
 
 export default DetailTwoScreen;

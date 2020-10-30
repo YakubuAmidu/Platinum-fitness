8445f727b31fc1bcb9237b530ee940e02c8d6030
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-30 01:27:23 -0700

    Commiting changes

--- src/screens/BottomTabs.js
+++ src/screens/BottomTabs.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { SimpleLineIcons, Foundation, Entypo } from "@expo/vector-icons";
 
 const BottomTabs = () => {
   return (
     <View style={styles.viewStyleOne}>
-      <View style={styles.viewStyleTwo}>
+      <TouchableOpacity style={styles.viewStyleTwo}>
         <SimpleLineIcons name="screen-smartphone" size={30} color="#1cafde" />
         <Text style={styles.textStyleThree}>My Day</Text>
-      </View>
-      <View style={styles.viewStyleTwo}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.viewStyleTwo}>
         <Foundation name="graph-bar" size={30} color="gray" />
         <Text style={styles.textStyleFour}>Goals</Text>
-      </View>
-      <View style={styles.viewStyleTwo}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.viewStyleTwo}>
         <SimpleLineIcons name="fire" size={30} color="#FF8C00" />
         <Text style={styles.textStyleFive}>Lets Go!</Text>
-      </View>
-      <View style={styles.viewStyleTwo}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.viewStyleTwo}>
         <Entypo name="news" size={30} color="gray" />
         <Text style={styles.textStyleSix}>News Feed</Text>
-      </View>
-      <View style={styles.viewStyleTwo}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.viewStyleTwo}>
         <Entypo name="user" size={30} color="gray" />
         <Text style={styles.textStyleSeven}>Profile</Text>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 };
